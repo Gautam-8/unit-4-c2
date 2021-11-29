bd8a68f064af9af854130c008ef75caf4cd3eb23
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-11-29 16:13:20 +0530

    Update company.controller.js

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -40,15 +40,7 @@ router.get("/:id" , async (req,res) => {
 })
 
 
-router.get("/most" , async (req,res) => {
 
-    try{
-        const company = await Company.find().sort({no_of_jobs:-1}).limit(1).lean().exec();
-    return res.status(200).send({company});
 
-    }catch(e){
-        return res.status(500).send( {message: e.message})
-    }
-})
 
 module.exports = router;
